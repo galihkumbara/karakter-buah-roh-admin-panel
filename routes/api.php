d056f71c8124ad243f07078c2cb702e643522d1a
HaningGRK <haning.grk@gmail.com>
2024-07-16 23:50:51 +0700

    a

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\ContentController;
+use App\Http\Controllers\MemberModuleController;
 use App\Http\Controllers\QuestionnaireController;
 use App\Http\Controllers\QuestionnaireQuestionMemberController;
 use App\Http\Controllers\TransactionController;
+use App\Models\MemberModule;
 use App\Models\QuestionnaireQuestionMember;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('/admin/transactions', TransactionController::class);
 Route::apiResource('/questionnaires', QuestionnaireController::class);
 Route::apiResource('/questionnairemembers', QuestionnaireQuestionMemberController::class);
+Route::apiResource('/membermodules', MemberModuleController::class);
+
 Route::apiResource('/contents', ContentController::class);
 
 
